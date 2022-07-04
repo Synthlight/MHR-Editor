0d615f8ad3c7dbabaf674104b2d28bcae485d568
Gregory Brutsche <gregory@mail.brutsches.local>
2022-07-04 11:17:44 -0400

    Add name lookup to layered armor.

---
+++ Generated/Manual/Structs/LayeredArmor.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using MHR_Editor.Common;
+using MHR_Editor.Common.Attributes;
+using MHR_Editor.Common.Data;
+
+namespace MHR_Editor.Models.Structs;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+public partial class Snow_equip_PlOverwearBaseUserData_Param {
+    private const uint layeredIdBitMask = 0x0FFF;
+    private const uint armorIdBitMask   = 0x000FFFFF;
+
+    [SortOrder(50)]
+    public string Name => DataHelper.ARMOR_SERIES_LOOKUP[Global.locale].TryGet((uint) Series);
+
+    public override string ToString() {
+        return Name;
+    }
+
+    public uint GetArmorId() {
+        return (uint) RelativeId & armorIdBitMask;
+    }
+
+    public uint GetLayeredId() {
+        return (uint) Id & layeredIdBitMask;
+    }
+}

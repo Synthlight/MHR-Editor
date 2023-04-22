9142c74c1d64b37d2023f2b6065534a673697720
Gregory Brutsche <gregory@mail.brutsches.local>
2023-04-21 22:29:21 -0400

    Fix build script targeting the wrong SLN.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
-MSBuild /nologo /t:Clean /p:Configuration=Debug MHR-Editor.sln
-MSBuild /nologo /t:Clean /p:Configuration=Release MHR-Editor.sln
-MSBuild /nologo /t:Build /p:Configuration=Debug MHR-Editor.sln
-MSBuild /nologo /t:Build /p:Configuration=Release MHR-Editor.sln
+MSBuild /nologo /t:Clean /p:Configuration=Debug RE-Editor.sln
+MSBuild /nologo /t:Clean /p:Configuration=Release RE-Editor.sln
+MSBuild /nologo /t:Build /p:Configuration=Debug RE-Editor.sln
+MSBuild /nologo /t:Build /p:Configuration=Release RE-Editor.sln
 pause

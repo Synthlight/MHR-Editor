8d789bda441413f4eca67c095a3f0d153f3f685e
Gregory Brutsche <gregory@mail.brutsches.local>
2023-04-21 20:21:53 -0400

    Set update check URLs.

--- Common/PathHelper.cs
+++ Common/PathHelper.cs
@@ -10,8 +10,8 @@ public static class PathHelper {
     public const string FLUFFY_MODS_PATH = @"R:\Games\Monster Hunter Rise\modmanager\Games\MHRISE\Mods";
     public const string RAR_SCRIPT       = @"/mnt/r/Games/Monster\ Hunter\ Rise/MHR-Editor/compress-rar.sh";
 
-    public const string NEXUS_URL              = "";
-    public const string JSON_VERSION_CHECK_URL = "";
+    public const string NEXUS_URL              = "https://www.nexusmods.com/monsterhunterrise/mods/114";
+    public const string JSON_VERSION_CHECK_URL = "http://brutsches.com/MHR-Editor.version.json";
 
     [SuppressMessage("ReSharper", "StringLiteralTypo")]
     public static readonly string[] TEST_PATHS = {

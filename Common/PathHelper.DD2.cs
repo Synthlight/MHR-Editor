4aef3037c11e6a968f1814bdcaafa746ba7b14b0
Gregory Brutsche <gregory@mail.brutsches.local>
2024-03-23 12:46:17 -0400

    Add method to create new shop items.

--- Common/PathHelper.DD2.cs
+++ Common/PathHelper.DD2.cs
@@ -23,9 +23,8 @@ public static class PathHelper {
         @"\natives\stm\appsystem\ch\common\human\userdata\",
     ];
 
-    public const string ARMOR_DATA_PATH                     = "natives/STM/AppSystem/Item/ItemData/ItemArmorData.user.2";
-    public const string DIFFICULTY_SETTINGS_PATH            = "natives/stm/appsystem/userdata/gamesystemuserdata.user.2";
-    public const string ITEM_DATA_PATH                      = "natives/STM/AppSystem/Item/ItemData/ItemData.user.2";
-    public const string WEAPON_DATA_PATH                    = "natives/STM/AppSystem/Item/ItemData/ItemWeaponData.user.2";
-    public const string DEFAULT_CHARACTER_STATUS_PARAM_PATH = "natives/stm/appsystem/ch/common/userdata/defaultcharacterstatusparametertable.user.2";
+    public const string ARMOR_DATA_PATH     = "natives/STM/AppSystem/Item/ItemData/ItemArmorData.user.2";
+    public const string ITEM_DATA_PATH      = "natives/STM/AppSystem/Item/ItemData/ItemData.user.2";
+    public const string ITEM_SHOP_DATA_PATH = "natives/STM/AppSystem/Item/ItemShopData/ItemShopData.user.2";
+    public const string WEAPON_DATA_PATH    = "natives/STM/AppSystem/Item/ItemData/ItemWeaponData.user.2";
 }
---
+++ Generated/Manual/DD2/Structs/ItemShop.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using RE_Editor.Common.Models;
+using RE_Editor.Models.Enums;
+
+namespace RE_Editor.Models.Structs;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+public partial class App_ItemShopBuyParam {
+    public static App_ItemShopBuyParam MakeNewBuyItem(RSZ rsz, int item, ushort stock) {
+        var newItem = Create(rsz);
+        newItem.ItemId                  = item;
+        newItem.PriceRateList           = [];
+        newItem.Stock                   = stock;
+        newItem.AddStockDays            = 1;
+        newItem.AddStockNum             = stock;
+        newItem.ReleaseQuestId          = App_QuestDefine_ID.Invalid;
+        newItem.QuestResultNo           = -1;
+        newItem.ReleaseSentimentRankBit = 1;
+        newItem.ReleaseTotalPay         = 0;
+        newItem.ReleaseFlag             = 0;
+        newItem.IsCheckJob              = false;
+        return newItem;
+    }
+}

9491c05c3517eff91a460868dd39a28f342ca3a8
Gregory Brutsche <gregory@mail.brutsches.local>
2024-03-23 13:43:57 -0400

    Add experimental "More Portcrystals" DD2 mod.

---
+++ Mods/DD2/MorePortCrystals.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using RE_Editor.Common;
+using RE_Editor.Common.Models;
+using RE_Editor.Constants;
+using RE_Editor.Models;
+using RE_Editor.Models.Structs;
+using RE_Editor.Util;
+
+namespace RE_Editor.Mods;
+
+[UsedImplicitly]
+[SuppressMessage("ReSharper", "SwitchStatementHandlesSomeKnownEnumValuesWithDefault")]
+[SuppressMessage("ReSharper", "ConvertSwitchStatementToSwitchExpression")]
+[SuppressMessage("ReSharper", "SwitchStatementMissingSomeEnumCasesNoDefault")]
+public class MorePortCrystals : IMod {
+    [UsedImplicitly]
+    public static void Make() {
+        const string bundleName  = "More Portcrystals Maybe";
+        const string description = "Changes the limit from 10 to 500. Experimental and untested.";
+        const string version     = "1.0";
+        const string outPath     = $@"{PathHelper.MODS_PATH}\{bundleName}";
+
+        var mod = new NexusMod {
+            Version = version,
+            Name    = bundleName,
+            Desc    = description,
+            Files = [
+                PathHelper.ITEM_PARAMETERS_PATH,
+            ],
+            Action = Mod
+        };
+
+        ModMaker.WriteMods([mod], PathHelper.CHUNK_PATH, outPath, bundleName, true, makeIntoPak: true);
+    }
+
+    public static void Mod(List<RszObject> rszObjectData) {
+        foreach (var obj in rszObjectData) {
+            switch (obj) {
+                case App_ItemParameters itemParameters:
+                    itemParameters.WarpPointPutEnableNum = 500;
+                    break;
+            }
+        }
+    }
+}

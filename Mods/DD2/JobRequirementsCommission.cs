3a9c7538d3209019aad67eac8f5151ce4d791118
Gregory Brutsche <gregory@mail.brutsches.local>
2024-03-24 19:16:06 -0400

    Add a "No Job Requirements (Commission)" mod for DD2.

---
+++ Mods/DD2/JobRequirementsCommission.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using JetBrains.Annotations;
+using RE_Editor.Common;
+using RE_Editor.Common.Models;
+using RE_Editor.Constants;
+using RE_Editor.Models;
+using RE_Editor.Models.Structs;
+using RE_Editor.Util;
+
+namespace RE_Editor.Mods;
+
+[UsedImplicitly]
+[SuppressMessage("ReSharper", "SwitchStatementHandlesSomeKnownEnumValuesWithDefault")]
+[SuppressMessage("ReSharper", "ConvertSwitchStatementToSwitchExpression")]
+[SuppressMessage("ReSharper", "SwitchStatementMissingSomeEnumCasesNoDefault")]
+public class JobRequirementsCommission : IMod {
+    [UsedImplicitly]
+    public static void Make() {
+        const string bundleName  = "No Job Requirements (Commission)";
+        const string description = "Tweaks weapon & armor data to let you use any armor/weapon with any job.";
+        const string version     = "1.1";
+        const string outPath     = $@"{PathHelper.MODS_PATH}\{bundleName}";
+
+        var baseMod = new NexusModVariant {
+            Version      = version,
+            NameAsBundle = "No Job Requirements",
+            Desc         = description,
+        };
+
+        var mods = new[] {
+            baseMod
+                .SetDesc("Commission: Armor with no weight + Seven Rings Enhanced (x10)")
+                .SetName("No Job Requirements: Armors Only (No Weight + Seven Rings Enhanced (x10))")
+                .SetFiles([PathHelper.ARMOR_DATA_PATH])
+                .SetAction(list => {
+                    JobRequirements.Jobs(list, JobRequirements.JobOptions.ARMOR_ONLY);
+                    ItemTweaks.Weight(list, ItemTweaks.WeightOptions._0);
+                    Mod(list);
+                }),
+        };
+
+        ModMaker.WriteMods(mods.ToList(), PathHelper.CHUNK_PATH, outPath, variantBundleName: bundleName, copyToFluffy: true, makeIntoPak: true);
+    }
+
+    public static void Mod(List<RszObject> rszObjectData) {
+        foreach (var obj in rszObjectData) {
+            switch (obj) {
+                case App_ItemArmorParam itemData:
+                    switch (itemData.Id) {
+                        case (int) ItemConstants.RING_OF_ARTICULACY:
+                            itemData.SpecialValue = 99;
+                            break;
+                        case (int) ItemConstants.RING_OF_ACCRUAL:
+                        case (int) ItemConstants.RING_OF_AMBITION:
+                        case (int) ItemConstants.RING_OF_ENDEAVOR:
+                        case (int) ItemConstants.RING_OF_AGGRESSION:
+                        case (int) ItemConstants.RING_OF_PERCIPIENCE:
+                            itemData.SpecialValue *= 10;
+                            break;
+                        case (int) ItemConstants.RING_OF_TRIUMPH:
+                            itemData.SpecialValue = itemData.SpecialValue2 = 1000;
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+}

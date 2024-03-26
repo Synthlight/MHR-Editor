f1e1406c188427f52006fe326d23e0561b7d662a
Gregory Brutsche <gregory@mail.brutsches.local>
2024-03-25 21:32:11 -0400

    Implement `via.Position`.

--- Common/Structs/Position.cs
+++ Common/Structs/Position.cs
@@ -5,15 +5,28 @@ using RE_Editor.Common.Models;
 namespace RE_Editor.Common.Structs;
 
 [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
-public class Position : RszObject, IViaType { // TODO
+public class Position : RszObject, IViaType {
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+
     public void Read(BinaryReader reader) {
+        X = reader.ReadDouble();
+        Y = reader.ReadDouble();
+        Z = reader.ReadDouble();
     }
 
     public void Write(BinaryWriter writer) {
+        writer.Write(X);
+        writer.Write(Y);
+        writer.Write(Z);
     }
 
     public Position Copy() {
         return new() {
+            X = X,
+            Y = Y,
+            Z = Z
         };
     }
 }

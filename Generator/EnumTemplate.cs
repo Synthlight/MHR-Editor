61eaa29874a12daf0c03a65cd4eeb6287aaf0c8d
Gregory Brutsche <gregory@mail.brutsches.local>
2024-04-06 02:29:02 -0400

    Add a generated enum flags list helper method.

--- Generator/EnumTemplate.cs
+++ Generator/EnumTemplate.cs
@@ -10,6 +10,8 @@ public class EnumTemplate(EnumType enumType) {
         using var file     = new StreamWriter(dryRun ? new MemoryStream() : File.Open(filename, FileMode.Create, FileAccess.Write));
 
         file.WriteLine("// ReSharper disable All");
+        file.WriteLine("using System.Diagnostics.Contracts;");
+        file.WriteLine("");
         file.WriteLine("namespace RE_Editor.Models.Enums;");
         file.WriteLine("");
         if (enumType.isFlags) {
@@ -17,6 +19,22 @@ public class EnumTemplate(EnumType enumType) {
         }
         file.Write($"public enum {enumType.name} : {enumType.type} ");
         file.Write(enumType.Contents);
+        if (enumType.isFlags) {
+            WriteExtensions(file);
+        }
         file.Close();
+    }
+
+    private void WriteExtensions(TextWriter file) {
+        file.WriteLine("");
+        file.WriteLine("");
+        file.WriteLine($"public static class {enumType.name}Extensions {{");
+        file.WriteLine("    [Pure]");
+        file.WriteLine($"    public static IEnumerable<{enumType.name}> GetFlags(this {enumType.name} values) {{");
+        file.WriteLine($"        return from {enumType.name} entry in Enum.GetValues(typeof({enumType.name}))");
+        file.WriteLine("               where (values & entry) != 0");
+        file.WriteLine("               select entry;");
+        file.WriteLine("    }");
+        file.Write("}");
     }
 }

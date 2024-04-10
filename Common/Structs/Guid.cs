603f30f717d7044fd3a4cfb23c7e84eebef23987
Gregory Brutsche <gregory@mail.brutsches.local>
2024-04-10 05:01:48 -0400

    Implement a type converter for `via.Guid`.

--- Common/Structs/Guid.cs
+++ Common/Structs/Guid.cs
@@ -1,21 +1,15 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
 using RE_Editor.Common.Models;
 
 namespace RE_Editor.Common.Structs;
 
 [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+[TypeConverter(typeof(GuidTypeConverter))]
 public class Guid : RszObject, ISimpleViaType {
     public System.Guid Value { get; set; }
-
-    [DisplayName("")]
-    public static ObservableCollection<Guid> NewIdInAList => [
-        new() {
-            Value = System.Guid.NewGuid()
-        }
-    ];
 
     public void Read(BinaryReader reader) {
         Value = new(reader.ReadBytes(16));
@@ -33,5 +27,23 @@ public class Guid : RszObject, ISimpleViaType {
 
     public override string ToString() {
         return Value.ToString();
+    }
+}
+
+public class GuidTypeConverter : TypeConverter {
+    public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType) {
+        return true;
+    }
+
+    public override bool CanConvertTo(ITypeDescriptorContext? context, Type? destinationType) {
+        return true;
+    }
+
+    public override object ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value) {
+        return new Guid {Value = System.Guid.Parse((string) value)};
+    }
+
+    public override object? ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType) {
+        return value?.ToString();
     }
 }

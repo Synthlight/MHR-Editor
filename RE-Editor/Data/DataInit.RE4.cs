3abfb8092283b156ca78496d75c79cb9629a7ce2
Gregory Brutsche <gregory@mail.brutsches.local>
2024-04-10 23:10:03 -0400

    Copy RE4 item lookup names across all the dicts because one is never complete by itself.

--- RE-Editor/Data/DataInit.RE4.cs
+++ RE-Editor/Data/DataInit.RE4.cs
@@ -7,21 +7,30 @@ namespace RE_Editor.Data;
 public static partial class DataInit {
     // ReSharper disable once IdentifierTypo
     private static void LoadDicts() {
-        DataHelper.ITEM_NAME_LOOKUP   = new();
-        DataHelper.WEAPON_NAME_LOOKUP = new();
+        DataHelper.ITEM_NAME_LOOKUP   = [];
+        DataHelper.WEAPON_NAME_LOOKUP = [];
 
         foreach (var variant in Global.VARIANTS) {
             DataHelper.ITEM_NAME_LOOKUP[variant]   = LoadDict<Global.LangIndex, Dictionary<uint, string>>(GetAsset($"{variant}_ITEM_NAME_LOOKUP"));
             DataHelper.WEAPON_NAME_LOOKUP[variant] = LoadDict<Global.LangIndex, Dictionary<uint, string>>(GetAsset($"{variant}_WEAPON_NAME_LOOKUP"));
         }
-        // Now 'fill in' the blanks with stuff from base without overwriting anything.
-        foreach (var (key, value) in DataHelper.ITEM_NAME_LOOKUP["CH"]) {
-            if (!DataHelper.ITEM_NAME_LOOKUP["AO"].ContainsKey(key)) DataHelper.ITEM_NAME_LOOKUP["AO"][key] = value;
-            if (!DataHelper.ITEM_NAME_LOOKUP["MC"].ContainsKey(key)) DataHelper.ITEM_NAME_LOOKUP["MC"][key] = value;
-        }
-        foreach (var (key, value) in DataHelper.WEAPON_NAME_LOOKUP["CH"]) {
-            if (!DataHelper.WEAPON_NAME_LOOKUP["AO"].ContainsKey(key)) DataHelper.WEAPON_NAME_LOOKUP["AO"][key] = value;
-            if (!DataHelper.WEAPON_NAME_LOOKUP["MC"].ContainsKey(key)) DataHelper.WEAPON_NAME_LOOKUP["MC"][key] = value;
+
+        // Scuffed, but fill in all potentially missing entries with the data from the others without overwriting anything.
+        // The game likes to share IDs across the modes and I don't know the rules.
+        FillMissing(DataHelper.ITEM_NAME_LOOKUP, ["CH", "AO", "MC"], ["CH", "AO", "MC"]);
+        FillMissing(DataHelper.WEAPON_NAME_LOOKUP, ["CH", "AO", "MC"], ["CH", "AO", "MC"]);
+    }
+
+    // ReSharper disable once ParameterTypeCanBeEnumerable.Local
+    private static void FillMissing(IReadOnlyDictionary<string, Dictionary<Global.LangIndex, Dictionary<uint, string>>> dict, string[] targets, string[] sources) {
+        foreach (var source in sources) {
+            foreach (var (language, entries) in dict[source]) {
+                foreach (var (id, text) in entries) {
+                    foreach (var target in targets) {
+                        dict[target][language].TryAdd(id, text);
+                    }
+                }
+            }
         }
     }
 }

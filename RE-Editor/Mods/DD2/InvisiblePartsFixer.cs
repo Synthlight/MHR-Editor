599af856e60471f52180275e908a499ac0ded80f
Gregory Brutsche <gregory@mail.brutsches.local>
2024-04-11 18:05:58 -0400

    Add an "Invisible Parts Fixer" option to null every fur msk texture file.

--- RE-Editor/Mods/DD2/InvisiblePartsFixer.cs
+++ RE-Editor/Mods/DD2/InvisiblePartsFixer.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using JetBrains.Annotations;
 using RE_Editor.Common;
 using RE_Editor.Common.Models;
@@ -16,18 +18,37 @@ public class InvisiblePartsFixer : IMod {
     public static void Make() {
         const string name        = "Invisible Parts Fixer";
         const string description = "Restores missing skin textures from mesh parts hidden by tweaking the armor swap database.";
-        const string version     = "1.0";
+        const string version     = "1.1";
 
-        var mod = new NexusMod {
-            Version         = version,
-            Name            = name,
-            Desc            = description,
-            Image           = $@"{PathHelper.MODS_PATH}\{name}\Title.png",
-            Files           = [],
-            AdditionalFiles = new() {{$@"{PathHelper.MODS_PATH}\{name}\InvisiblePartsFixer.lua", @"reframework\autorun\InvisiblePartsFixer.lua"}},
-            SkipPak         = true,
+        var baseMod = new NexusMod {
+            Version      = version,
+            Desc         = description,
+            NameAsBundle = name,
+            Image        = $@"{PathHelper.MODS_PATH}\{name}\Title.png",
+            Files        = [],
         };
 
-        ModMaker.WriteMods([mod], name, copyLooseToFluffy: true, noPakZip: true);
+        var nulledMasks = new Dictionary<string, string>();
+        var reader      = File.OpenText(@"V:\DD2\DD2_PC_Release.list");
+        while (!reader.EndOfStream) {
+            var line = reader.ReadLine()!
+                             .ToLower()
+                             .Replace('\\', '/');
+            if (line.Contains("/_furmasks/")) {
+                nulledMasks[line] = $@"{PathHelper.MODS_PATH}\{name}\null furmask.tex.760230703";
+            }
+        }
+
+        var mods = new List<NexusMod> {
+            baseMod.SetName($"{name} (REF)")
+                   .SetDesc($"{description}\nWorks on-the-fly with player equipment. Doesn't work for invisible parts in the inventory or photo mode.")
+                   .SetAdditionalFiles(new() {{@"reframework\autorun\InvisiblePartsFixer.lua", $@"{PathHelper.MODS_PATH}\{name}\InvisiblePartsFixer.lua"}})
+                   .SetSkipPak(true),
+            baseMod.SetName($"{name} (Blank All Fur Masks)")
+                   .SetDesc($"{description}\nBlanks every single fur mask in the game, which works for photo mode & the inventory too.")
+                   .SetAdditionalFiles(nulledMasks),
+        };
+
+        ModMaker.WriteMods(mods, name, copyLooseToFluffy: true);
     }
 }

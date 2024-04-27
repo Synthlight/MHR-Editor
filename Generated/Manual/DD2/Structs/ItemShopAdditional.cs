bb4d0527494f1c62ab7d8c2a483e4e2dae994685
Gregory Brutsche <gregory@mail.brutsches.local>
2024-04-27 11:56:54 -0400

    Add names to the item shop additional DD2 file.

---
+++ Generated/Manual/DD2/Structs/ItemShopAdditional.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using RE_Editor.Common;
+using RE_Editor.Common.Attributes;
+using RE_Editor.Common.Data;
+
+namespace RE_Editor.Models.Structs;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+public partial class App_ItemShopAdditionalParam {
+    [SortOrder(50)]
+    public string Name => DataHelper.ITEM_NAME_LOOKUP[Global.locale].TryGet((uint) Id);
+}
